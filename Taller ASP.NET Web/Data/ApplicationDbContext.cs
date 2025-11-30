73e01324918a4b72a73933e5628c2b2a546ddfe6
juan francisco martinez azuara <a2223330175@alumnos.uat.edu.mx>
2025-11-30 12:55:07 -0600

    Agregar archivos de proyecto.

---
+++ Taller ASP.NET Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Taller_ASP.NET_Web.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        // DbSet para la tabla Tasks
+        public DbSet<Models.TaskItem> TaskItems { get; set; }
+    }
+}
---
+++ Taller ASP.NET Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Taller_ASP.NET_Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
